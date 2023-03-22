bdaf9a091dac8981fec6031d18c562d2c2ba9860
WHYZNSoftware <whyzn.software@gmail.com>
2023-03-22 21:35:30 +0100

    Ordnerstruktur

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 
 Future<void> main() async {
   //Firebase Connection
