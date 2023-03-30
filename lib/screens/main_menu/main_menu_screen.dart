f51c418eb7eb0f8117bbbc7e07daa3e821fb35b4
Philipp Kremling <philipp.kremling.pk@gmail.com>
2023-03-30 15:59:39 +0200

    Centralized mock data

--- lib/screens/main_menu/main_menu_screen.dart
+++ lib/screens/main_menu/main_menu_screen.dart
@@ -1,10 +1,13 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:oobacht/screens/main_menu/pages/main_list/main_list.dart';
 import 'package:oobacht/screens/main_menu/pages/main_map/main_map.dart';
 import 'package:oobacht/screens/new_report/new_report_screen.dart';
 
 import '../../../../utils/navigator_helper.dart' as navigator;
+import '../../logic/classes/group.dart';
+import '../../logic/classes/report.dart';
 import 'drawer/main_menu_drawer.dart';
 
 class MainMenuScreen extends StatefulWidget {
@@ -21,7 +24,7 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
   //PageController to make pages swipeable
   final _pageViewController = PageController();
   int _activePageIndex = 0;
-  final List<Widget> _contentPages = [const MainMap(), const MainList()];
+  final List<Widget> _contentPages = [MainMap(reports: _getMockReports()), MainList(reports: _getMockReports())];
 
   @override
   void dispose() {
@@ -107,5 +110,43 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
   void _newReport() {
     navigator.navigateToNewScreen(
         newScreen: const NewReportScreen(), context: context);
+  }
+
+  static List<Report> _getMockReports() {
+    return [
+      Report(
+          "1",
+          "Krasse Meldung",
+          "Ich hab etwas wirklich krasses gefunden, deshalb muss ich erst mal richtig viel Text drüber schreiben um meine UI testen zu können!",
+          DateTime.now(),
+          [
+            Group("1", "Mathematiker", Icons.add, Colors.blue),
+            Group("2", "Speicherwütiger!", Icons.save, Colors.green),
+            Group("3", "Was auch immer?!", Icons.person, Colors.blueGrey),
+            Group("4", "Gute Frage", Icons.ten_k, Colors.yellow),
+          ],
+          const LatLng(48.545166, 8.706739),
+          "http://"),
+      Report(
+          "2",
+          "Richtig langer Name der Meldung was geht denn hier ab??!?!?",
+          "Diese Meldung hat kein Bild hinterlegt, darum keine Anzeige oben!",
+          DateTime.now(),
+          [
+            Group("1", "Mathematiker", Icons.add, Colors.blue),
+          ],
+          const LatLng(48.445166, 8.606739),
+          ""),
+      Report(
+          "3",
+          "Dritte Meldung, die komplett mit ihrem Titel übers Ziel hinaus schießt und hoffentlich richtig angezeigt wird",
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et e",
+          DateTime.now(),
+          [
+            Group("5", "Uhrwerker", Icons.watch, Colors.red),
+          ],
+          const LatLng(48.745166, 8.706739),
+          "http://"),
+    ];
   }
 }
--- lib/screens/main_menu/pages/main_map/main_map.dart
+++ lib/screens/main_menu/pages/main_map/main_map.dart
@@ -12,56 +12,15 @@ import '../../../../utils/map_utils.dart';
 import 'components/customgooglemap.dart';
 
 class MainMap extends StatefulWidget {
-  const MainMap({Key? key}) : super(key: key);
+  const MainMap({Key? key, required this.reports}) : super(key: key);
+
+  final List<Report> reports;
 
   @override
   _MainMapState createState() => _MainMapState();
 }
 
-// TODO get reports from backend
-final List<Report> _reportsMOCK = [
-  Report(
-      "0",
-      "Test 0",
-      "Desc 0",
-      DateTime.now(),
-      [Group("pets", "Gefahr für Tiere", Icons.pets, Colors.brown),
-        Group("general", "Allgemeine Gefahr", Icons.dangerous_rounded, Colors.red)],
-      const LatLng(48.445166, 8.706739),
-      ""),
-  Report(
-      "1",
-      "Test 1",
-      "Desc 1",
-      DateTime.now(),
-      [
-        Group("childs", "Gefahr für Kinder", Icons.child_friendly, Colors.yellow),
-        Group("pets", "Gefahr für Tiere", Icons.pets, Colors.brown)
-      ],
-      const LatLng(48.445166, 8.686739),
-      ""),
-  Report(
-      "2",
-      "Test 2",
-      "Desc 2",
-      DateTime.now(),
-      [
-        Group("general", "Allgemeine Gefahr", Icons.dangerous_rounded, Colors.red)
-      ],
-      const LatLng(48.445166, 8.676739),
-      ""),
-  Report(
-      "3",
-      "Test 3",
-      "Desc 3",
-      DateTime.now(),
-      [Group("climber", "Gefahr für Kletterer", Icons.sports, Colors.blue)],
-      const LatLng(48.445166, 8.666739),
-      ""),
-];
-
 class _MainMapState extends State<MainMap> {
-  List<Report> reportsList = _reportsMOCK;
   Position? _currentPosition;
   late GoogleMapController mapController;
   Widget _widget = const Center(child: CircularProgressIndicator());
@@ -71,7 +30,7 @@ class _MainMapState extends State<MainMap> {
   Future<void> _createMap() async {
     final theme = Theme.of(context);
 
-    _markers = await generateMarkers(reportsList, theme, context);
+    _markers = await generateMarkers(widget.reports, theme, context);
 
     _currentPosition = await getCurrentPosition(context);
     setState(() {
@@ -81,7 +40,7 @@ class _MainMapState extends State<MainMap> {
           CustomGoogleMap(
               currentPosition: _currentPosition,
               markers: _markers),
-          MapCaption(reportsList: reportsList)
+          MapCaption(reportsList: widget.reports),
         ],
       );
     });
