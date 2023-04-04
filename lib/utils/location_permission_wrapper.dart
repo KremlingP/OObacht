7c68e0b8f34f621872cf0f8f458da53090e3a78f
WHYZNSoftware <whyzn.software@gmail.com>
2023-04-04 22:53:38 +0200

    updated styling and removed false comments

--- lib/utils/location_permission_wrapper.dart
+++ lib/utils/location_permission_wrapper.dart
@@ -10,10 +10,12 @@ class LocationPermissionWrapper extends StatefulWidget {
   const LocationPermissionWrapper({Key? key}) : super(key: key);
 
   @override
-  _LocationPermissionWrapperState createState() => _LocationPermissionWrapperState();
+  _LocationPermissionWrapperState createState() =>
+      _LocationPermissionWrapperState();
 }
 
-class _LocationPermissionWrapperState extends State<LocationPermissionWrapper> with WidgetsBindingObserver {
+class _LocationPermissionWrapperState extends State<LocationPermissionWrapper>
+    with WidgetsBindingObserver {
   bool locationPermissionGranted = false;
 
   @override
@@ -25,12 +27,30 @@ class _LocationPermissionWrapperState extends State<LocationPermissionWrapper> w
       future: initialRequestPermission(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return const ErrorText(text: "Fehler beim Auslesen der Berechtigungen!");
+          return Scaffold(
+              backgroundColor: theme.colorScheme.background,
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.info_outline,
+                    color: Colors.red,
+                    size: 100,
+                  ),
+                  SizedBox(height: 50),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    child: ErrorText(
+                        text:
+                            "Fehler beim Auslesen der Standort-Berechtigungen, bitte aktiviere diese in den Optionen oder starte ggf. die App neu!"),
+                  ),
+                ],
+              ));
         }
         if (!locationPermissionGranted) {
-          return Container(
-            color: Colors.orange,
-            child: Column(
+          return Scaffold(
+            backgroundColor: Colors.orange,
+            body: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -40,26 +60,29 @@ class _LocationPermissionWrapperState extends State<LocationPermissionWrapper> w
                   width: data.size.shortestSide / 1.4,
                   color: Colors.white,
                 ),
-                Text("OObacht! benötigt immer Berechtigungen auf deinen Standort, um dir relevante Meldungen zu senden...",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: theme.primaryColor,
-                    fontSize: 20,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Text(
+                    "OObacht! benötigt immer Berechtigungen auf deinen Standort, um dir relevante Meldungen zu senden, bitte aktiviere diese in den Einstellungen!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: theme.colorScheme.background,
-                    foregroundColor: theme.colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: theme.colorScheme.background,
+                      foregroundColor: theme.colorScheme.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
-                  ),
-                  onPressed: () async {
+                    onPressed: () async {
                       await requestPermission();
-                  },
-                  child: const Text('Berechtigungen erteilen')
-                )
+                    },
+                    child: const Text('Berechtigungen erteilen'))
               ],
             ),
           );
@@ -71,8 +94,6 @@ class _LocationPermissionWrapperState extends State<LocationPermissionWrapper> w
 
   Future<void> initialRequestPermission() async {
     PermissionStatus result;
-    // In Android we need to request the storage permission,
-    // while in iOS is the photos permission
     if (Platform.isAndroid) {
       result = await Permission.locationAlways.request();
     } else {
@@ -89,8 +110,6 @@ class _LocationPermissionWrapperState extends State<LocationPermissionWrapper> w
 
   Future<void> requestPermission() async {
     PermissionStatus result;
-    // In Android we need to request the storage permission,
-    // while in iOS is the photos permission
     if (Platform.isAndroid) {
       result = await Permission.locationAlways.request();
     } else {
@@ -113,7 +132,7 @@ class _LocationPermissionWrapperState extends State<LocationPermissionWrapper> w
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     // If user resumed to this app, check permission
-    if(state == AppLifecycleState.resumed) {
+    if (state == AppLifecycleState.resumed) {
       requestPermission();
     }
   }
