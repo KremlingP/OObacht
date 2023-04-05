dc1b827599510fe75f98ebd00b5f65024078d693
Philipp Kremling <philipp.kremling.pk@gmail.com>
2023-04-05 16:04:06 +0200

    Backend call

--- lib/firebase/functions/notification_functions.dart
+++ lib/firebase/functions/notification_functions.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:oobacht/firebase/url_helper.dart';
 
 class NotificationFunctions {
   static Future<void> updateFcmToken(String fcmToken) async {
     HttpsCallable callable = FirebaseFunctions.instance.httpsCallableFromUrl(
-      'https://europe-west1-oobacht-ea4d4.cloudfunctions.net/oobacht',
-    ); // TODO Correct URL
+      UrlHelper.getFunctionUrl("updateFcmToken"),
+    );
 
     final response = await callable.call(<String, dynamic>{
       'fcmToken': fcmToken,
