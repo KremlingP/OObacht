84e299a38543057b9dfbd6da1a5654a2b0f68bd5
Philipp Kremling <philipp.kremling.pk@gmail.com>
2023-04-12 13:06:07 +0200

    WIP

--- lib/utils/json_serialization/date_time_converter.dart
+++ lib/utils/json_serialization/date_time_converter.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 //TODO implement DateTimeConverter, update Map and newDate
 
-class DateTimeConverter implements JsonConverter<DateTime, Map<String, int>> {
+class DateTimeConverter implements JsonConverter<DateTime, Map<String, Map<String,int>>> {
   const DateTimeConverter();
 
   @override
@@ -10,6 +10,6 @@ class DateTimeConverter implements JsonConverter<DateTime, Map<String, int>> {
 
   @override
   Map<String, int> toJson(DateTime dateTime) {
-    return <String, int>{'_day': dateTime.day, '_usw': dateTime.hour};
+    return <String, int>{'_nanoseconds': dateTime., '_usw': dateTime.hour};
   }
 }
